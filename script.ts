9abd4543d181c20b881144bc0b45894098bbe05c
DusanSutulovic <sutulovic.dusan@gmail.com>
2024-11-24 11:34:22 +0100

    Class DockerCOntainerHandler added

--- script.ts
+++ script.ts
@@ -1,60 +1,55 @@
 import { spawn } from 'child_process';
 
-async function startDockerContainer(composePath: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const dockerProcess = spawn('docker', ['compose', '-f', composePath, 'up', '-d']);
+export class DockerContainerHandler {
+    constructor(private composePath) {}
+    
+    private handleDockerProcess(optionsAdd: string[]): Promise<void> {
+        return new Promise((resolve, reject) => {
+        const options: string[] = ['compose', '-f'].concat(optionsAdd);
+        const dockerProcess = spawn('docker', options);
+        dockerProcess.stdout.on('data', (data) => {
+            console.log(`Docker Log: ${data}`);
+            });
+    
+        dockerProcess.stderr.on('data', (data) => {
+            console.log(`Docker Log: ${data}`);
+            });
 
-    dockerProcess.stdout.on('data', (data) => {
-      console.log(`Docker Log: ${data}`);
-    });
+        dockerProcess.on('close', (code) => {
+            if (code === 0) {
+                console.log(`Container ${this.composePath} started successfully.`);
+                resolve();
+            } else {
+                reject(new Error(`Docker process exited with code ${code}`));
+            }
+            });
 
-    dockerProcess.stderr.on('data', (data) => {
-      console.log(`Docker Log: ${data}`);
-    });
+        });
+    }
 
-    dockerProcess.on('close', (code) => {
-      if (code === 0) {
-        console.log(`Container ${composePath} started successfully.`);
-        resolve();
-      } else {
-        reject(new Error(`Docker process exited with code ${code}`));
-      }
-    });
-  });
+    startDockerContainer() {
+        return this.handleDockerProcess([this.composePath, 'up', '-d']);
+    }
+    
+    endDockerContainer(removeVolumes: boolean) {
+            let options: string[] = [this.composePath, 'down'];
+
+            if (removeVolumes) {
+                options.push('-v');
+            }
+
+            return this.handleDockerProcess(options);
+        }
+
 }
 
-async function endDockerContainer(composePath: string, removeVolumes: boolean): Promise<void> {
-    return new Promise((resolve, reject) => {
-      let dockerProcess;
-      if (removeVolumes) {
-        dockerProcess = spawn('docker', ['compose', '-f', composePath, 'down', '-v']);
-      } else {
-        dockerProcess = spawn('docker', ['compose', '-f', composePath, 'down']);
-      }
-      
-  
-      dockerProcess.stdout.on('data', (data) => {
-        console.log(`Docker Log: ${data}`);
-      });
-  
-      dockerProcess.stderr.on('data', (data) => {
-        console.log(`Docker Log: ${data}`);
-      });
-  
-      dockerProcess.on('close', (code) => {
-        if (code === 0) {
-          console.log(`Container ${composePath} stopped successfully.`);
-          resolve();
-        } else {
-          reject(new Error(`Docker process exited with code ${code}`));
-        }
-      });
-    });
-  }
 
-// Esempio di utilizzo
+
+/* Esempio di utilizzo
 startDockerContainer('./docker_container/example2.yaml')
   .then(() => {endDockerContainer('./docker_container/example2.yaml', true)
                 .then(() => console.log('Container terminato!'))
                 .catch((err) => console.error('Errore:', err));})
   .catch((err) => console.error('Errore:', err));
+
+  */
